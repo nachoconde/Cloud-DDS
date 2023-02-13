cd363365d57d6d8df364dbef442611a87f266946
nachoconde <nachok@gmail.com>
2023-02-13 12:02:35 +0100

    Multicast and Unicast configurations

--- Zenoh/server/scripts/ros2servers.sh
+++ Zenoh/server/scripts/ros2servers.sh
@@ -2,4 +2,5 @@
 source /ros_entrypoint.sh &
 ros2 run demo_nodes_cpp add_two_ints_server &
 ros2 run examples_rclpy_minimal_action_server server &
+ros2 run demo_nodes_cpp listener &
 tail -f /dev/null
